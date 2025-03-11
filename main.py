02ffe26546742d7e06eadcb9e920680617fc5130
R4DIOAKTIV <hattanyossri@yahoo.com>
2025-03-11 06:19:19 +0200

    "beda3"

--- main.py
+++ main.py
@@ -41,21 +41,66 @@ def get_ROI_edge(edge_img, img):
     
     return masked
 
+# Hough Transform
 def hough_transform(image):
-    # accumulation into ro, theta space
+    height, width = image.shape
+    max_dist = int(np.hypot(height, width))
+    thetas = np.deg2rad(np.arange(0, 180))
+    rhos = np.linspace(-max_dist, max_dist, max_dist * 2)
 
-    # refining coordinates
-    pass
+    accumulator = np.zeros((len(rhos), len(thetas)), dtype=np.int32)
+    y_idxs, x_idxs = np.nonzero(image)
+    for i in range(len(x_idxs)):
+        x = x_idxs[i]
+        y = y_idxs[i]
+        for t_idx in range(len(thetas)):
+            rho = int(x * np.cos(thetas[t_idx]) + y * np.sin(thetas[t_idx]) + max_dist)
+            accumulator[rho, t_idx] += 1
+
+    # Plot the accumulator array
+    cv2.imshow('Accumulator', accumulator / np.max(accumulator))
+    cv2.waitKey(0)
+
+    # Non-maximum suppression
+    threshold = 0.1 * np.max(accumulator)
+    peaks = np.argwhere(accumulator > threshold)
+    refined_peaks = np.zeros_like(accumulator)
+
+    for peak in peaks:
+        rho, theta = peak
+        if accumulator[rho, theta] == np.max(accumulator[rho-1:rho+2, theta-1:theta+2]):
+            refined_peaks[rho, theta] = accumulator[rho, theta]
+
+    return refined_peaks
+
+def draw(img, peaks):
+    height, width = img.shape[:2]
+    for peak in np.argwhere(peaks):
+        rho, theta = peak
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    cv2.imshow('Lines', img)
+    cv2.waitKey(0)
     
 def main():
     test_img = cv2.imread('test.jpg')
     #if image too large, resize    
     blurried_img = median_blur(test_img)
     edge_img = canny_edge_detector(blurried_img)
-    roi_img = get_ROI_edge(edge_img, test_img)
+    roi_img = get_ROI_edge(edge_img, test_img)  
+    refined_peaks = hough_transform(roi_img)
+    draw(test_img, refined_peaks)
     cv2.imshow('ROI', roi_img)
     cv2.imshow('Edge', edge_img)
     cv2.imshow('Blur', blurried_img)
+    cv2.imshow('refined peaks', refined_peaks)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
